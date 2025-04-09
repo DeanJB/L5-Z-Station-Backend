b58f1f0dbc7c01bc0c17b844ce186e8e29bca9a0
Dean Barrow <deanjordanbarrow@gmail.com>
2025-04-09 21:13:13 +1200

    backend

--- server.js
+++ server.js
@@ -2,43 +2,57 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
-const port = process.env.PORT || 5000;
+const Products = require("./schema");
 
 dotenv.config();
+const PORT = process.env.PORT || 3000;
 
-const dbURI = process.env.MONGODB_URI;
+// const dbURI = process.env.MONGODB_URI;
 
-// Connect to MongoDB Atlas
+// Connect to MongoDB Compass
 mongoose
-      .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-      .then(() => console.log("MongoDB Connected"))
-      .catch((err) => console.log(err));
+      .connect("mongodb://127.0.0.1:27017/Products-Z-Station")
+      .then(() => {
+            console.log("MongoDB Connected Successfully");
+      })
+      .catch((err) => console.log("MongoDB Connection Error:", err));
 
-// Schema
-const productsSchema = new mongoose.Schema({
-      name: String,
-      image: String,
+app.listen(PORT, () => {
+      console.log(`server running on ${PORT} `);
 });
-
-const Products = mongoose.model("products", productsSchema);
 
 // Dean API endpoint
 app.get("/api/products/:productId", async (req, res) => {
       try {
-            const manyProducts = await Products.find();
-            res.json(manyProducts);
+            const product = await Products.findById(req.params.productId);
+            if (!product) {
+                  return res.status(404).json({ message: "Product not found" });
+            }
+            res.json(product);
       } catch (err) {
             res.status(500).json({ message: err.message });
       }
 });
-// >>>>>>>>>>>>>>>>>>>
 
-// Mac API endpoint
+//
+app.get("/api/products", async (req, res) => {
+      const category = req.query.category;
 
-// >>>>>>>>>>>>>>>>>>>
+      // All categories
+      const validCategories = ["Hot Drink", "Cold Drink", "Savoury", "Vegetarian"];
+      if (!validCategories.includes(category)) {
+            return res.status(400).json({ message: "Invalid category" });
+      }
 
-// Hamish API endpoint
+      try {
+            const products = await Products.find({ category }).select("name image");
 
-// >>>>>>>>>>>>>>>>>>>
+            if (!products || products.length === 0) {
+                  return res.status(404).json({ message: `No ${category} products found` });
+            }
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+            res.json(products);
+      } catch (err) {
+            res.status(500).json({ message: err.message });
+      }
+});
